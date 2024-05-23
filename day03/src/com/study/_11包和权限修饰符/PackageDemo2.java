7117f0dbec0ac39acd8012e387af5739b945a065
whale-coding <njq3699@gmail.com>
2024-05-23 14:45:24 +0800

    day03_包和权限修饰符

---
+++ day03/src/com/study/_11包和权限修饰符/PackageDemo2.java
@@ -0,0 +1,24 @@
+package com.study._11包和权限修饰符;
+
+/**
+ * @Classname: PackageDemo2
+ * @Date: 2024/5/23 14:30
+ * @Author: 聂建强
+ * @Description:
+ *
+ *     目标：权限修饰符。
+ *
+ *     权限修饰符：有四种（private -> 缺省 -> protected - > public ）
+ *     可以修饰成员变量，修饰方法，修饰构造器，内部类，不同修饰符修饰的成员能够被访问的权限将受到限制!
+ *
+ *     四种修饰符的访问权限范围：
+ *                         private    缺省       protected   public
+ *         本类中           √           √          √           √
+ *         本包下其他类中    X           √          √           √
+ *         其他包下的类中    X           X          X           √
+ *         其他包下的子类中  X           X          √           √
+ *
+ *
+ */
+public class PackageDemo2 {
+}
---
+++ day03/src/com/study/_11包和权限修饰符/Student.java
@@ -0,0 +1,13 @@
+package com.study._11包和权限修饰符;
+
+/**
+ * @Classname: Student
+ * @Date: 2024/5/23 14:26
+ * @Author: 聂建强
+ * @Description:
+ */
+public class Student {
+    public static void inAddr(){
+        System.out.println("我在合肥安徽大学");
+    }
+}
